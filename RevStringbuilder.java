0daead53e4f58f3bec1c45e5edf5f0efdf92131b
Thoshinijalakam <thoshinijalakam@gmail.com>
2025-10-08 18:30:12 +0530

    Add files via upload

---
+++ RevStringbuilder.java
@@ -0,0 +1,7 @@
+public class RevStringBuilder{
+    public static void main(String[] args) {
+        String str="Hello World";
+        StringBuilder sb=new StringBuilder(str);
+        System.out.println("Reveresed:"+sb.reverse());
+    }
+}
